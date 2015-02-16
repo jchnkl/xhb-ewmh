2ec23a227a1424b64f36e47d61d2a34cc61fec23
Jochen Keil <jochen.keil@gmail.com>
2015-02-16 13:26:29 +0100

    Use unsafeLookupUp, because EWMH_ATOMs are preseeded

--- src/Graphics/XHB/Ewmh/Class.hs
+++ src/Graphics/XHB/Ewmh/Class.hs
@@ -10,24 +10,22 @@ module Graphics.XHB.Ewmh.Class
     ( MonadEwmh(..)
     ) where
 
-import qualified Data.Map as M
-import Control.Monad.State (modify, gets)
 import Control.Monad.Reader (ask)
 import Control.Monad.Trans (MonadTrans(..))
 import Control.Monad.IO.Class (MonadIO(..))
-import Graphics.XHB (Connection, SomeError, ATOM, InternAtom(..))
-import qualified Graphics.XHB as X
+import Graphics.XHB (Connection, ATOM)
 import qualified Graphics.XHB.Atom as XA
+import Graphics.XHB.Ewmh.Atoms
 import Graphics.XHB.Ewmh.Types
 
 class MonadIO m => MonadEwmh m where
-    getAtom :: String -> m (Either SomeError ATOM)
+    getEwmhAtom :: EWMH_ATOM -> m ATOM
     getConnection :: m Connection
 
 instance MonadIO m => MonadEwmh (EwmhT m) where
-    getAtom = EwmhT . XA.lookupAtom
+    getEwmhAtom = EwmhT . XA.unsafeLookupAtom . toAtomString
     getConnection = EwmhT ask
 
 instance (MonadTrans t, MonadEwmh m, MonadIO (t m)) => MonadEwmh (t m) where
-    getAtom = lift . getAtom
+    getEwmhAtom = lift . getEwmhAtom
     getConnection = lift getConnection
