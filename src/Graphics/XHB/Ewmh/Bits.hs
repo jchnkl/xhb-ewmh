6dc7b08ae842a472b885f4d51d5bc798fc69418f
Jochen Keil <jochen.keil@gmail.com>
2015-02-22 13:05:39 +0100

    Add additional data structures for complex properties
    Needs some orphan instances for xhb types.

--- src/Graphics/XHB/Ewmh/Bits.hs
+++ src/Graphics/XHB/Ewmh/Bits.hs
@@ -1,9 +1,11 @@
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE StandaloneDeriving #-}
 
 module Graphics.XHB.Ewmh.Values where
 
+import Data.Word (Word32)
 import Data.Typeable (Typeable)
-import Graphics.XHB (BitEnum(..))
+import Graphics.XHB (BitEnum(..), ButtonIndex(..), StackMode(..), WINDOW)
 
 data NET_DESKTOP_LAYOUT_ORIENTATION = NET_WM_ORIENTATION_HORZ
                                     | NET_WM_ORIENTATION_VERT
@@ -39,21 +41,29 @@ instance BitEnum NET_DESKTOP_LAYOUT_STARTING_CORNER where
         3 -> NET_WM_BOTTOMLEFT
         _ -> error "NET_DESKTOP_LAYOUT_STARTING_CORNER: no such bit"
 
-data NET_WM_MOVERESIZE_DIRECTION = NET_WM_MOVERESIZE_SIZE_TOPLEFT
-                                 | NET_WM_MOVERESIZE_SIZE_TOP
-                                 | NET_WM_MOVERESIZE_SIZE_TOPRIGHT
-                                 | NET_WM_MOVERESIZE_SIZE_RIGHT
-                                 | NET_WM_MOVERESIZE_SIZE_BOTTOMRIGHT
-                                 | NET_WM_MOVERESIZE_SIZE_BOTTOM
-                                 | NET_WM_MOVERESIZE_SIZE_BOTTOMLEFT
-                                 | NET_WM_MOVERESIZE_SIZE_LEFT
-                                 | NET_WM_MOVERESIZE_MOVE
-                                 | NET_WM_MOVERESIZE_SIZE_KEYBOARD
-                                 | NET_WM_MOVERESIZE_MOVE_KEYBOARD
-                                 | NET_WM_MOVERESIZE_CANCEL
+data NetDesktopLayout = NetDesktopLayout
+    { orientation     :: NET_DESKTOP_LAYOUT_ORIENTATION
+    , starting_corner :: NET_DESKTOP_LAYOUT_STARTING_CORNER
+    , columns         :: Word32
+    , rows            :: Word32
+    }
+    deriving (Eq, Ord, Read, Show, Typeable)
+
+data NetWmMoveresizeDirection = NET_WM_MOVERESIZE_SIZE_TOPLEFT
+                             | NET_WM_MOVERESIZE_SIZE_TOP
+                             | NET_WM_MOVERESIZE_SIZE_TOPRIGHT
+                             | NET_WM_MOVERESIZE_SIZE_RIGHT
+                             | NET_WM_MOVERESIZE_SIZE_BOTTOMRIGHT
+                             | NET_WM_MOVERESIZE_SIZE_BOTTOM
+                             | NET_WM_MOVERESIZE_SIZE_BOTTOMLEFT
+                             | NET_WM_MOVERESIZE_SIZE_LEFT
+                             | NET_WM_MOVERESIZE_MOVE
+                             | NET_WM_MOVERESIZE_SIZE_KEYBOARD
+                             | NET_WM_MOVERESIZE_MOVE_KEYBOARD
+                             | NET_WM_MOVERESIZE_CANCEL
     deriving (Enum, Eq, Ord, Read, Show, Typeable)
 
-instance BitEnum NET_WM_MOVERESIZE_DIRECTION where
+instance BitEnum NetWmMoveresizeDirection where
     toBit v = case v of
         NET_WM_MOVERESIZE_SIZE_TOPLEFT     -> 0
         NET_WM_MOVERESIZE_SIZE_TOP         -> 1
@@ -100,48 +110,97 @@ instance BitEnum NET_WM_STATE_ACTION where
         2 -> NET_WM_STATE_TOGGLE
         _ -> error "NET_WM_STATE_ACTION: no such bit"
 
-data SOURCE_INDICATION = SOURCE_NONE
-                       | SOURCE_APPLICATION
-                       | SOURCE_PAGER
+data SourceIndication = SourceNone
+                      | SourceApplication
+                      | SourcePager
     deriving (Enum, Eq, Ord, Read, Show, Typeable)
 
-instance BitEnum SOURCE_INDICATION where
+instance BitEnum SourceIndication where
     toBit v = case v of
-        SOURCE_NONE        -> 0
-        SOURCE_APPLICATION -> 1
-        SOURCE_PAGER       -> 2
+        SourceNone        -> 0
+        SourceApplication -> 1
+        SourcePager       -> 2
 
     fromBit v = case v of
-        0 -> SOURCE_NONE
-        1 -> SOURCE_APPLICATION
-        2 -> SOURCE_PAGER
-        _ -> error "SOURCE_INDICATION: no such bit"
+        0 -> SourceNone
+        1 -> SourceApplication
+        2 -> SourcePager
+        _ -> error "SourceIndication: no such bit"
 
-data NET_MOVERESIZE_WINDOW_FLAG = NET_MOVERESIZE_WINDOW_X
-                                | NET_MOVERESIZE_WINDOW_Y
-                                | NET_MOVERESIZE_WINDOW_WIDTH
-                                | NET_MOVERESIZE_WINDOW_HEIGHT
-                                | NET_MOVERESIZE_WINDOW_SOURCE SOURCE_INDICATION
+data Gravity = WinGravity
+             | NorthWest
+             | North
+             | NorthEast
+             | West
+             | Center
+             | East
+             | SouthWest
+             | South
+             | SouthEast
+             | Static
+    deriving (Enum, Eq, Ord, Read, Show, Typeable)
+
+instance BitEnum Gravity where
+    toBit v = case v of
+        WinGravity -> 0
+        NorthWest  -> 1
+        North      -> 2
+        NorthEast  -> 3
+        West       -> 4
+        Center     -> 5
+        East       -> 6
+        SouthWest  -> 7
+        South      -> 8
+        SouthEast  -> 9
+        Static     -> 10
+
+    fromBit v = case v of
+        0  -> WinGravity
+        1  -> NorthWest
+        2  -> North
+        3  -> NorthEast
+        4  -> West
+        5  -> Center
+        6  -> East
+        7  -> SouthWest
+        8  -> South
+        9  -> SouthEast
+        10 -> Static
+        _  -> error "Gravity: no such bit"
+
+data NetMoveresizeWindow = NetMoveresizeWindow
+    { netMoveresizeWindow_sourceIndication :: SourceIndication
+    , netMoveresizeWindow_gravity          :: Gravity
+    , netMoveresizeWindow_x                :: Maybe Int
+    , netMoveresizeWindow_y                :: Maybe Int
+    , netMoveresizeWindow_width            :: Maybe Word32
+    , netMoveresizeWindow_height           :: Maybe Word32
+    }
     deriving (Eq, Ord, Read, Show, Typeable)
 
--- 8 ^= x, 9 ^= x, 10 ^= width, 11 ^= height, 12 ^= application, 13 ^= pager
-instance BitEnum NET_MOVERESIZE_WINDOW_FLAG where
-    -- toBit :: a -> Int
-    toBit d = case d of
-        NET_MOVERESIZE_WINDOW_X                         -> 8
-        NET_MOVERESIZE_WINDOW_Y                         -> 9
-        NET_MOVERESIZE_WINDOW_WIDTH                     -> 10
-        NET_MOVERESIZE_WINDOW_HEIGHT                    -> 11
-        NET_MOVERESIZE_WINDOW_SOURCE SOURCE_APPLICATION -> 12
-        NET_MOVERESIZE_WINDOW_SOURCE SOURCE_PAGER       -> 13
-        f -> error $ "NET_MOVERESIZE_WINDOW_FLAG: no bit for " ++ show f
+-- TODO: push to xhb package
+deriving instance Eq ButtonIndex
+deriving instance Ord ButtonIndex
+deriving instance Read ButtonIndex
 
-    -- fromBit :: Int -> a
-    fromBit i = case i of
-        8 ->  NET_MOVERESIZE_WINDOW_X
-        9 ->  NET_MOVERESIZE_WINDOW_Y
-        10 -> NET_MOVERESIZE_WINDOW_WIDTH
-        11 -> NET_MOVERESIZE_WINDOW_HEIGHT
-        12 -> NET_MOVERESIZE_WINDOW_SOURCE SOURCE_APPLICATION
-        13 -> NET_MOVERESIZE_WINDOW_SOURCE SOURCE_PAGER
-        b  -> error $ "NET_MOVERESIZE_WINDOW_FLAG: no such bit: " ++ show b
+data NetWmMoveresize = NetWmMoveresize
+    { netWmMoveresize_x_root           :: Maybe Int
+    , netWmMoveresize_y_root           :: Maybe Int
+    , netWmMoveresize_direction        :: NetWmMoveresizeDirection
+    , netWmMoveresize_button           :: ButtonIndex
+    , netWmMoveresize_sourceIndication :: SourceIndication
+    }
+    deriving (Eq, Ord, Read, Show, Typeable)
+
+-- TODO: push to xhb package
+deriving instance Eq StackMode
+deriving instance Ord StackMode
+deriving instance Read StackMode
+
+-- no Read because there's no Read instance for WINDOW
+data NetRestackWindow = NetRestackWindow
+    { netRestackWindow_sourceIndication :: SourceIndication
+    , netRestackWindow_sibling_window   :: WINDOW
+    , netRestackWindow_detail           :: StackMode
+    }
+    deriving (Eq, Ord, Show, Typeable)
