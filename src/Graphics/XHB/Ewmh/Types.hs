c93ead3b701e9bd9504983e7f174d835c667e3a7
Jochen Keil <jochen.keil@gmail.com>
2015-02-23 13:01:50 +0100

    Just an type alias for MonadEwmh and EwmhT

--- src/Graphics/XHB/Ewmh/Types.hs
+++ src/Graphics/XHB/Ewmh/Types.hs
@@ -1,41 +1,15 @@
-{-# LANGUAGE FlexibleInstances          #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
-{-# LANGUAGE UndecidableInstances       #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE ConstraintKinds    #-}
 
-module Graphics.XHB.Ewmh.Types (Ewmh, EwmhT(..)) where
+module Graphics.XHB.Ewmh.Types where
 
-import Control.Applicative (Applicative)
-import Control.Monad.State (MonadState(..))
-import Control.Monad.Reader (MonadReader(..), ReaderT(..))
-import Control.Monad.Writer (MonadWriter(..))
-import Control.Monad.Trans (MonadTrans(..))
-import Control.Monad.IO.Class (MonadIO)
-import Graphics.XHB (Connection)
+import Data.Typeable (Typeable)
 import Graphics.XHB.Atom
+import Graphics.XHB.Ewmh.Atoms
 
-newtype EwmhT m a = EwmhT { unEwmhT :: ReaderT Connection (AtomT m) a }
-    deriving ( Applicative
-             , Functor
-             , Monad
-             , MonadIO
-             )
+type EwmhT = AtomT
 
 type Ewmh = EwmhT IO
 
-instance MonadTrans EwmhT where
-    lift m = EwmhT . lift . lift $ m
+type MonadEwmh = MonadAtom
 
-instance (MonadState s m) => MonadState s (EwmhT m) where
-    get = lift get
-    put = lift . put
-    state = lift . state
-
-instance (MonadReader r m) => MonadReader r (EwmhT m) where
-    ask = lift ask
-    local f (EwmhT m) = EwmhT . ReaderT $ local f . runReaderT m
-
-instance (MonadWriter w m) => MonadWriter w (EwmhT m) where
-    tell = lift . tell
-    listen = EwmhT . listen . unEwmhT
-    pass = EwmhT . pass . unEwmhT
