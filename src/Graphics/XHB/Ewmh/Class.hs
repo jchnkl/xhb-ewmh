e892916315780df650d8ade867f2c911a710517f
Jochen Keil <jochen.keil@gmail.com>
2015-02-23 13:31:17 +0100

    Archive

--- src/Graphics/XHB/Ewmh/Class.hs
+++ src/Graphics/XHB/Ewmh/Class.hs
@@ -7,25 +7,32 @@
 {-# LANGUAGE UndecidableInstances       #-}
 
 module Graphics.XHB.Ewmh.Class
-    ( MonadEwmh(..)
-    ) where
+    where
+    -- ( MonadEwmh(..)
+    -- ) where
 
 import Control.Monad.Reader (ask)
 import Control.Monad.Trans (MonadTrans(..))
 import Control.Monad.IO.Class (MonadIO(..))
 import Graphics.XHB (Connection, ATOM)
-import qualified Graphics.XHB.Atom as XA
+import Graphics.XHB.Atom
 import Graphics.XHB.Ewmh.Atoms
 import Graphics.XHB.Ewmh.Types
 
-class MonadIO m => MonadEwmh m where
-    getEwmhAtom :: EWMH_ATOM -> m ATOM
-    getConnection :: m Connection
+-- instance MonadAtom 
 
-instance MonadIO m => MonadEwmh (EwmhT m) where
-    getEwmhAtom = EwmhT . XA.unsafeLookupAtom . toAtomString
-    getConnection = EwmhT ask
-
-instance (MonadTrans t, MonadEwmh m, MonadIO (t m)) => MonadEwmh (t m) where
-    getEwmhAtom = lift . getEwmhAtom
-    getConnection = lift getConnection
+-- class MonadAtom m => MonadEwmh m where
+-- -- class (AtomNameLike l, Monad m) => MonadAtom l m where
+--     insertAtom :: l -> ATOM -> m ()
+--     lookupAtom :: l -> m (Maybe ATOM)
+--     lookupName :: ATOM -> m (Maybe l)
+--     -- getEwmhAtom :: EWMH_ATOM -> m ATOM
+--     -- getConnection :: m Connection
+-- --
+-- -- instance MonadIO m => MonadEwmh (EwmhT m) where
+-- --     getEwmhAtom = EwmhT . XA.unsafeLookupAtom . toAtomString
+-- --     getConnection = EwmhT ask
+-- --
+-- -- instance (MonadTrans t, MonadEwmh m, MonadIO (t m)) => MonadEwmh (t m) where
+-- --     getEwmhAtom = lift . getEwmhAtom
+-- --     getConnection = lift getConnection
